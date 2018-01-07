d0e99f9df2c5da0629e035d4516bd76cb65e1d0b
Jérémy <jtonneau@antidot.com>
2018-01-07 12:59:26 +0100

    wip

--- deploy.php
+++ deploy.php
@@ -10,7 +10,7 @@ require 'vendor/deployer/recipes/recipe/npm.php';
 set('repository', 'git@github.com:Cryde/cr.git');
 
 set('git_tty', true); // [Optional] Allocate tty for git on first deployment
-set('keep_releases', 2);
+set('keep_releases', 1);
 add('shared_files', []);
 add('shared_dirs', []);
 add('writable_dirs', []);
